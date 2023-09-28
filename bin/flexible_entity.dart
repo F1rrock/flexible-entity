9b5b53e2f415229c3648cdb148b2278ebcebe288
F1rrock <flyrock000@gmail.com>
2023-09-28 21:09:05 +0800

    first commit

---
+++ bin/flexible_entity.dart
@@ -0,0 +1,5 @@
+import 'package:flexible_entity/flexible_entity.dart' as flexible_entity;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flexible_entity.calculate()}!');
+}
---
+++ test/flexible_entity_test.dart
@@ -0,0 +1,8 @@
+import 'package:flexible_entity/flexible_entity.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
